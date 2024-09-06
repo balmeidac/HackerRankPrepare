1ff75cac5639b1202076ec5acb6dad1cb6e88217
Byron Omar Almeida Coello <byronalmeidaec@gmail.com>
2024-09-05 22:40:43 -0500

    Update WeatherObservationStation6.sql

--- SQL/BasicSelect/WeatherObservationStation6.sql
+++ SQL/BasicSelect/WeatherObservationStation6.sql
@@ -1,4 +1,4 @@
-/* /* Weather Observation Station 6
+/* Weather Observation Station 6
 Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
 
 Input Format
